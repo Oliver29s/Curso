8c51f9cbdc8d8b737c8ae7ca9e6a8d7a35e3b263
oliver29s <mawiis455@gmail.com>
2022-08-12 11:45:12 -0500

    agregue css. titulo parrafo

---
+++ js/javas.js
@@ -0,0 +1,7 @@
+Seis tipos de datos primitivos, controlados por el operador typeof
+Undefined: typeof instance === "undefined"
+Boolean: typeof instance === "boolean"
+Number: typeof instance === "number"
+String: typeof instance === "string"
+BigInt: typeof instance === "bigint"
+Symbol: typeof instance === "symbol"
